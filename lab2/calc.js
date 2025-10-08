e7ab7535b079820c2f9e2116e30e347a7f9994ac
removvi <dmutroyeremov@gmail.com>
2025-10-08 22:12:53 +0300

    Create calc.js

---
+++ lab2/calc.js
@@ -0,0 +1,25 @@
+function average(a, b) {
+  return (a + b) / 2;
+}
+
+function square(x) {
+  return x * x;
+}
+
+function cube(x) {
+  return x * x * x;
+}
+
+function calculate() {
+  const results = [];
+  for (let i = 0; i <= 9; i++) {
+    const sq = square(i);
+    const cb = cube(i);
+    const avg = average(sq, cb); //среднее число
+    results.push(avg);
+    console.log(i + " : " + avg);
+  }
+  return results;
+}
+
+calculate();
