59362ac4b0c356d080a25ad9cbd910167fee24af
removvi <dmutroyeremov@gmail.com>
2025-10-08 19:10:43 +0300

    Create cycle1.js

---
+++ lab2/cycle1.js
@@ -0,0 +1,10 @@
+"use strict";
+//Example 1
+function range(start, end){
+    const array = [];
+    for (let i = start; i <= end; i++) {
+       array.push(i);  
+}
+return array;
+}
+console.log(range(15, 30));
