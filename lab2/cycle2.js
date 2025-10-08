b8f99e5ac7ef40ec8388c27b31ec00a735ba580f
removvi <dmutroyeremov@gmail.com>
2025-10-08 19:15:56 +0300

    Create cycle2.js

---
+++ lab2/cycle2.js
@@ -0,0 +1,10 @@
+function rangeOdd(start, end) {
+  const result = [];
+  for (let i = start; i <= end; i++) {
+    if (i % 2 !== 0) {
+      result.push(i);
+    }
+  }
+  return result;
+}
+console.log(rangeOdd(15, 30));
