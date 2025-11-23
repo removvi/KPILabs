fc4896214445693ce9512a35cee46699dc1af882
removvi <dmutroyeremov@gmail.com>
2025-11-24 01:13:01 +0200

    Create export2.js

---
+++ lab6/export2.js
@@ -0,0 +1,17 @@
+const bad = x => {
+  if (x > 3) throw new Error("Too big!");
+  return x * 2;
+};
+
+const add = x => x + 1;
+
+const f = composeRight(bad, add, add); 
+
+f.on('error', e => {
+  console.log("Error:", e.message);
+});
+
+console.log(f(1)); 
+console.log(f(5)); 
+
+export default pipe;
