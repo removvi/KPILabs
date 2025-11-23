db70ec77e4a0a7a679d4356e22038534f0fc9645
removvi <dmutroyeremov@gmail.com>
2025-11-23 21:29:31 +0200

    Create max.js

---
+++ lab4/max.js
@@ -0,0 +1,11 @@
+function max(matrix) {
+  let m = -Infinity;
+  for (const row of matrix) {
+    for (const value of row) {
+      if (value > m) m = value;
+    }
+  }
+  return m;
+}
+const m = max([[1, 2, 3], [4, 5, 6], [7, 8, 9]]);
+console.log(m); 
