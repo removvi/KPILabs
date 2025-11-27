924ec41fdaa66dc9e649d5451d620a10e819d95f
removvi <dmutroyeremov@gmail.com>
2025-11-27 18:55:45 +0200

    Create iterate.js

---
+++ lab8/iterate.js
@@ -0,0 +1,13 @@
+function iterate(object, callback) {
+  for (const key in object) {
+    if (Object.prototype.hasOwnProperty.call(object, key)) {
+      callback(key, object[key], object);
+    }
+  }
+}
+const obj = { a: 1, b: 2, c: 3 };
+
+iterate(obj, (key, value) => {
+  console.log({ key, value });
+});
+
