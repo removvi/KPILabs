a26ad80596a768b0c88054030b5c2ea8fbb273dd
removvi <dmutroyeremov@gmail.com>
2025-11-27 18:53:33 +0200

    Add difference function to compare two arrays

---
+++ lab7/difference.js
@@ -0,0 +1,6 @@
+function difference(array1, array2) {
+  return array1.filter(item => !array2.includes(item));
+}
+const array1 = [7, -2, 10, 5, 0];
+const array2 = [0, 10];
+console.log(difference(array1, array2));
